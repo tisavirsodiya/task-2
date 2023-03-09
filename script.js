785daf778e780e19b27371a9ba5c3c44a85cb920
tisa virsodiya <virsodiyatisa@gmail.com>
2023-03-09 19:37:31 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function submit()
+{
+    var dataentered = retrievedata();
+}
+
+function retrievedata()
+{
+    var name= document.getElementById("name").Value;
+    var Email= document.getElementById("Email").Value;
+    var phonenumber= document.getElementById("phonenumber").Value;
+    var birthdaydate= document.getElementById("birthdaydate").Value;
+    var appointmentime= document.getElementById("appointmenttime").Value;
+    var favoritecolor= document.getElementById("favoritecolor").Value;
+    var paymentmethod= document.getElementById("paymentmethod").Value;
+    var age= document.getElementById("age").Value;
+    var checkboxoption= document.getElementById("checkboxoption").Value;
+    var uploadphoto= document.getElementById("uploadphoto").Value;
+ 
+    return
+}
