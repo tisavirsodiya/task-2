4096f0222db4b159883b08d1cbe17a1380b1dcd9
tisa virsodiya <virsodiyatisa@gmail.com>
2023-03-10 23:42:38 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function submitform()
 {
     var dataEntered = retrievedata();
-    console.log(dataEntered);
+    // console.log(dataEntered);
      var readData=readingDataFromLocalStorage(dataEntered);
+    //  console.log(readData); 
+    insert(readData);
 }
+// CREAT
 // dataenter in form
 function retrievedata()
 {
@@ -21,6 +24,7 @@ function retrievedata()
     var arr=[Name,Email,phonenumber,birthdaydate,appointmenttime,favoritecolor,paymentmethod,age,checkboxoption,uploadphoto]
     return arr;
 }
+// READ
 // data in localstorage
 function readingDataFromLocalStorage(dataEntered) 
 {
@@ -35,4 +39,22 @@ function readingDataFromLocalStorage(dataEntered)
     var c=localStorage.setItem("checkboxoption", dataEntered[8]);
     var u=localStorage.setItem("uploadphoto", dataEntered[9]);
 
+    // getting values from local to table 
+    var n1=localStorage.getItem("Name",n);
+     var pn1=localStorage.getItem("phonenumber",pn);
+    var b1=localStorage.getItem("birthdaydate",b);
+    var a1=localStorage.getItem("appointmenttime",a);
+    var f1=localStorage.getItem("favoritecolor",f);
+    var p1=localStorage.getItem("payment", p);
+    var a1=localStorage.getItem("age", a);
+    var c1=localStorage.getItem("checkboxoption",c);
+    var u1=localStorage.getItem("uploadphoto", u);
+
+    var arr=[n1,pn1,b1,a1,f1,p1,a1,c1,u1]
+    return arr;
+}
+// INSERT
+function insert(readData)
+{
+    var row= table.insertRow();
 }
